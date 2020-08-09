53a0e5884e828b3911a1ea99a97a39ec9af45903
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-08-09 07:01:22 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
-<?php 
-  abstract class user_info
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>deltoon</title>
+  </head>
+  <body>
+  <?php
+  class login
   {
-    abstract protected function showMessage();
-  }
-  class login extends user_info
-  {
-    public function showMessage()
+    public function __construct()
     {
       date_default_timezone_set("Asia/Tehran");
       echo("<h1>".'welcom to our server'."</h1>");
       echo(date("D M d h:i:s o"))."<br/>";
       echo("ip:\t". $_SERVER["REMOTE_ADDR"])."<br/>";
+      echo("browser information:\t". $_SERVER["HTTP_USER_AGENT"])."<br/>";
     }
   }
   $user = new login();
-  $user -> showMessage();
-?>
+  ?>
+  </body>
+</html>
+
