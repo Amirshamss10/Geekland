f20bc0d27cea23f3d8c365b7930805fffaa90311
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-08-09 06:52:27 +0430

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     {
       date_default_timezone_set("Asia/Tehran");
       echo("<h1>".'welcom to our server'."</h1>");
-      echo(date("o/m/d g:i:s A"))."<br/>";
-      echo("ip:\t". $_SERVER["REMOTE_ADDR"])."<br/>";
+      echo(date("D M d h:i:s o")).PHP_EOL;
+      echo("ip:\t". $_SERVER["REMOTE_ADDR"]).PHP_EOL;
     }
   }
   $user = new login();
