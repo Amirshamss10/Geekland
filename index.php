1f2897161c5900b2d9d489b7010a6166b5d4ad10
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-08-09 06:24:27 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  abstract class user_info
+  {
+    abstract protected function showMessage();
+  }
+  class login extends user_info
+  {
+    public function showMessage()
+    {
+      date_default_timezone_set("Asia/Tehran");
+      echo("<h1>".'welcom to our server'."</h1>");
+      echo(date("o/m/d g:i:s A"))."<br/>";
+    }
+  }
+  $user = new login();
+  $user -> showMessage();
+?>
