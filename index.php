7e75f794fd8de37e5e0e1baf4c410a879191af0d
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-11-21 02:26:59 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <title>deltoon</title>
-  </head>
-  <body>
-  <?php
-  abstract class index
-  {
-    protected $ip;
-    protected $header;
-    protected $date;
-    abstract protected function save_info();
-  }
-  class show_info extends index
-  {
-    protected function save_info()
-    {
-      date_default_timezone_set("Asia/Tehran");
-      $this -> date = date("D M d h:i:s o");
-      $this -> ip = $_SERVER["REMOTE_ADDR"];
-      $this -> header = $_SERVER["HTTP_USER_AGENT"];
-      file_put_contents("user.txt","$this->ip \n $this->header \n $this->date \n-----------------",FILE_APPEND);
-    }
-    public function __construct()
-    {
-      $this -> save_info();
-      echo("<h1>".'welcom to our server'."</h1>");
-      echo($this -> date)."<br/>";
-      echo("ip:\t". $this -> ip)."<br/>";
-      echo("browser information:\t". $this -> header)."<br/>";
-    }
-  }
-  $user = new show_info();
-  ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"> 
+    <title>text hash</title> 
+    <style>
+        body{
+            background-color:yellow; 
+        }
+        h1{
+            text-align:center ;
+            color:green;
+        } 
+        form{
+            text-align: center;
+        }
+        p{
+            text-align: center; 
+            line-height: 20px; 
+            font: 18px tahoma;
+            color: blue;
+        }
+        </style>
+</head>
+<body>
+    <h1>welcom to our service</h1>
+    <form action="" method="post">
+        <input type="text" name="text" placeholder="text">
+        <input type="submit" name="submit" value="send">
+    </form>
+    <?php 
+  if(isset($_POST["submit"])) { 
+      $hash_admin = "2918157391eb2f1b4088692b81f2b0913781777c4051fa44b32ee878df887779"; //hash username admin! @AmirShamss
+      $text = strip_tags($_POST["text"]);   
+      if(hash('sha256',$text) == $hash_admin) {
+          echo("welcom to our admin")."<br/>";
+          echo("text:".$text)."<br/>";
+        } else {
+            echo("<p>hash:\t".hash('sha256',$text)."</p>")."<br/>";
+            echo("<p>text: $text</p>");
+        }
+    } 
+    ?>
   </body>
-</html>
-
-
+  </html>
+  
