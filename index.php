31883a99e9fa68aafc3146f406ec5f94ac2152bc
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-11-21 04:04:50 +0330

    Update index.php

--- index.php
+++ index.php
@@ -28,26 +28,21 @@
             color: #008080;
         } 
         input{
-            width: 30%;
-            padding: 10px 10px;
+            width: 100%;
+            padding: 20px 5px;
             margin: 10px 0;
             display: inline-block;
             border: 3px solid #ccc;
             border-radius: 30px;
         }
-        input#sender{
-            color: green;            
-        }
-        p#owner{
-            color: green;
-        }
+       
         </style>
 </head>
 <body>
     <h1>welcom to our service</h1>
     <form action=""  method="post">
         <input type="text" name="text" placeholder="Writing..."><br>
-        <input id=sender type="submit" name="submit" value="send">
+        <input  type="submit" name="submit" value="send" style=color:green>
     </form>
     <?php 
   if(isset($_POST["submit"])) { 
@@ -60,11 +55,10 @@
             echo("<p>text: $text</p>");
         } 
     } else {
-        echo "<p id='owner'><a href='https://telegram.me/userlandApp'>ساخته شده توسط یوزرلند</p>";
+        echo "<p id='owner'><a href='https://telegram.me/userlandApp' style=color:gray>ساخته شده توسط یوزرلند</p>";
     } 
     
     ?>
  
 </body>
   </html>
-   
