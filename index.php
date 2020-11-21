883b09141d0e8bdacfc77b515548b69541d603d8
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-11-21 03:37:14 +0330

    Update index.php

--- index.php
+++ index.php
@@ -14,33 +14,58 @@
         } 
         form{
             text-align: center;
+            padding:11px; 
+            font-size:20px;
+        }
+        a{
+            text-decoration:none!important;
         }
         p{
             text-align: center; 
             line-height: 20px; 
             font: 18px tahoma;
-            color: blue;
+            line-height : 0px;
+            color: #008080;
+        } 
+        input{
+            width: 30%;
+            padding: 10px 10px;
+            margin: 10px 0;
+            display: inline-block;
+            border: 3px solid #ccc;
+            border-radius: 30px;
+        }
+        input#sender{
+            color: green;            
+        }
+        p#owner{
+            color: green;
         }
         </style>
 </head>
 <body>
     <h1>welcom to our service</h1>
-    <form action="" method="post">
-        <input type="text" name="text" placeholder="text">
-        <input type="submit" name="submit" value="send">
+    <form action=""  method="post">
+        <input type="text" name="text" placeholder="Writing..."><br>
+        <input id=sender type="submit" name="submit" value="send">
     </form>
     <?php 
   if(isset($_POST["submit"])) { 
       $hash_admin = "2918157391eb2f1b4088692b81f2b0913781777c4051fa44b32ee878df887779"; //hash username admin! @AmirShamss
       $text = strip_tags($_POST["text"]);   
       if(hash('sha256',$text) == $hash_admin) {
-          echo("<script>alert('Hello world')</script>");
+          echo("<p>welcom to our admin</p>")."<br/>";
+          echo("<p>text:".$text."</p>")."<br/>";
         } else {
-            echo("<p>hash:\t".hash('sha256',$text)."</p>")."<br/>";
+            echo("<p>".hash('sha256',$text)."</p>")."<br/>";
             echo("<p>text: $text</p>");
-        }
+        } 
+    } else {
+        echo "<p id='owner'><a href='https://telegram.me/userlandApp'>ساخته شده توسط یوزرلند</p>";
     } 
+    
     ?>
-  </body>
+ 
+</body>
   </html>
-  
+   
