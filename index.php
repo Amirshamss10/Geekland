daf65ee1d35bc0f0ced7caabf1ff189aa9bc7fe3
Amirhossein Shams <61677001+Amirshamss10@users.noreply.github.com>
2020-12-16 00:21:23 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php 
-  if(isset($_POST["name"])) {
-    echo("hello $_POST['name']"); 
-  } else {
-    echo("404 not found");
-  }
-?>
+<?php $c = curl_init(); curl_setopt($c , CURLOPT_URL , "http://domain2.ir/newp/WebService.php"); curl_setopt($c , CURLOPT_POST , true); curl_setopt($c , CURLOPT_POSTFIELDS , "name=ali&family=asadi"); $result = curl_exec($c); curl_close($c); ?>
+
+
+
